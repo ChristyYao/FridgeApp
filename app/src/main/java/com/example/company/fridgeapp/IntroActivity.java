c88e05da7120b8bd05d55fc8af20f57174786604
iyyel <i@iyyel.io>
2019-01-12 14:46:18 +0100

    Added IntroActivity.

--- app/src/main/java/com/example/company/fridgeapp/IntroActivity.java
+++ app/src/main/java/com/example/company/fridgeapp/IntroActivity.java
@@ -1,14 +1,13 @@
 package com.example.company.fridgeapp;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.intro_activity);
     }
-
 }
