7ab701eba9512b8b30f2552c09bd26d77b7c2c47
iyyel <i@iyyel.io>
2019-01-12 15:54:28 +0100

    Added IntroActivity. Working now. :D

--- app/src/main/java/com/example/company/fridgeapp/IntroActivity.java
+++ app/src/main/java/com/example/company/fridgeapp/IntroActivity.java
@@ -1,13 +1,68 @@
 package com.example.company.fridgeapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.model.SliderPage;
 
-public class IntroActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class IntroActivity extends AppIntro {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.intro_activity);
+
+        SliderPage sliderPage1 = new SliderPage();
+        sliderPage1.setTitle("Welcome to FridgeApp");
+        sliderPage1.setDescription("Where food is kept safe.");
+        sliderPage1.setImageDrawable(R.drawable.appintro_indicator_dot_white);
+        sliderPage1.setBgColor(getResources().getColor(R.color.colorAccent));
+        addSlide(AppIntroFragment.newInstance(sliderPage1));
+
+        SliderPage sliderPage2 = new SliderPage();
+        sliderPage2.setTitle("Food");
+        sliderPage2.setDescription("Food Text Here");
+        sliderPage2.setImageDrawable(R.drawable.appintro_indicator_dot_white);
+        sliderPage2.setBgColor(getResources().getColor(R.color.colorAccent));
+        addSlide(AppIntroFragment.newInstance(sliderPage2));
+
+        SliderPage sliderPage3 = new SliderPage();
+        sliderPage3.setTitle("Fridge");
+        sliderPage3.setDescription("Fridge Stuff Here.");
+        sliderPage3.setImageDrawable(R.drawable.appintro_indicator_dot_white);
+        sliderPage3.setBgColor(getResources().getColor(R.color.colorAccent));
+        addSlide(AppIntroFragment.newInstance(sliderPage3));
+
+        SliderPage sliderPage4 = new SliderPage();
+        sliderPage4.setTitle("Freezer");
+        sliderPage4.setDescription("Freezer Stuff Here");
+        sliderPage4.setImageDrawable(R.drawable.appintro_indicator_dot_white);
+        sliderPage4.setBgColor(getResources().getColor(R.color.colorAccent));
+        addSlide(AppIntroFragment.newInstance(sliderPage4));
+
+        SliderPage sliderPage7 = new SliderPage();
+        sliderPage7.setTitle("You're all set");
+        sliderPage7.setDescription("Welcome to FridgeApp.");
+        sliderPage7.setImageDrawable(R.drawable.appintro_indicator_dot_white);
+        sliderPage7.setBgColor(getResources().getColor(R.color.colorAccent));
     }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent i = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent i = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/company/fridgeapp/MainActivity.java
+++ app/src/main/java/com/example/company/fridgeapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.company.fridgeapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        introCheck();
+    }
+
+    public void introCheck() {
+        /* Uncomment this line to get the intro back */
+        PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().remove("firstStart").apply();
+
+        /* Declare a new thread to do a preference check */
+        Thread thread = new Thread(() -> {
+
+            /* Initialize SharedPreferences */
+            SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+            /* Create a new boolean and preference and set it to true */
+            boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+            /* If the activity has never started before... */
+            if (isFirstStart) {
+                /* Launch app intro */
+                runOnUiThread(() -> startActivity(new Intent(this, IntroActivity.class)));
+
+                /* Make a new preferences editor */
+                SharedPreferences.Editor e = getPrefs.edit();
+
+                /* Edit preference to make it false because we don't want this to run again */
+                e.putBoolean("firstStart", false);
+
+                /* Apply the changes */
+                e.apply();
+            }
+        });
+
+        /* Start the thread */
+        thread.start();
     }
 
 }
+
+
